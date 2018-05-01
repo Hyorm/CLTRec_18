d04dc9cd7c7d5f1424f342bb64de707896d4dddf
hyorm <hyorm1226@gmail.com>
2018-05-02 05:07:20 +0900

    Closet and Clothes .java

--- src/cr/closet/Closet.java
+++ src/cr/closet/Closet.java
@@ -7,6 +7,58 @@ import java.awt.*;
 
 public class Closet{
 
+	private Map<int, Clothes> closet = new Map<int, Clothes>();
 	
+	public Closet(){}
 
+	public Closet(Clothes clothes){
+		this.closet.put(clothes.getProductId(), clothes);
+	}
+	
+	public void getClothesMap(){
+		
+		Set<String> keySet = closet.keySet();
+
+		Iterator<String> iterator = keySet.iterator();
+
+		while(iterator.hasNext()) {
+ 
+			int key = iterator.next();
+			Clothes value = closet.get(key);
+
+			System.out.printf("productId : %s , ClothesFeature : %d %n", key, value.getFeature());
+		}
+
+		//TODO:return
+	}
+
+	public Clothes getClothes(int productId){
+
+		return closet.get(productId);
+	}
+
+	public void setClothesMap(Clothes clothes){
+
+		this.closet.put(clothes.getProductId(), clothes);
+
+	}
+
+	public void rmClothes(int productId){
+
+		this.closet.remove(productId);
+
+	}
+
+	@overload
+	public void rmClothes(Clothes clothes){
+	
+		this.closet.remove(clothes.getProductId());
+
+	}
+
+	public void initClothesMap(){
+	
+		this.closet.clear();
+		
+	}
 }
---
+++ src/cr/closet/Clothes.java
@@ -0,0 +1,61 @@
+package cr.closet;
+  
+import cr.*;
+
+import java.util.*;
+import java.awt.*;
+
+public class Clothes{
+
+	public Clothes(){}
+
+	public Clothes(int productId){
+	
+		this.productId = productId;
+
+	}
+
+	public Clothes(int productId, int type, int category, int gender, int color, int season, int style){
+		this.feature = this.feature + tenToBin(type);
+		this.feature = this.feature + tenToBin(category);
+		this.feature = this.feature + tenToBin(gender);
+		this.feature = this.feature + tenToBin(color);
+		this.feature = this.feature + tenToBin(season);
+		this.feature = this.feature + tenToBin(style);
+
+		if((feature.length()>24)||bin.length()<0)
+                        System.out.println("Invalid Feature Information");
+	}
+
+	private String feature = "";
+
+	private int productId;
+
+	public String getFeature(){
+
+		return this.feature;
+	}
+
+	public int getProductId(){
+		
+		return this.productId;
+		
+	}
+
+	public String tenToBin(int ten){
+
+		String bin = "";
+
+		while (ten > 1) {
+
+			bin = ten % 2 + bin;
+			ten = ten / 2;
+		}
+		bin = ten + bin;
+
+		if((bin.length()>4)||bin.length()<0)
+			System.out.println("Invalid bin Information");
+	
+		return bin;
+	}
+}
