ef5f2b59a4fc73a48f5b87ab71e2800d1ef9b805
21500214 <21500214@handong.edu>
2018-05-02 21:16:11 +0900

    Update User.java

--- src/cr/usr/User.java
+++ src/cr/usr/User.java
@@ -6,7 +6,34 @@ import java.util.*;
 import java.awt.*;
 
 public class User{
-
-
+	private String userId;
+	private String userPW;
+	private String type;
+	private int age;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
