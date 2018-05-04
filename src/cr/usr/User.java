166be08b0de915513d9d7d70b11894d4674f45b3
Hyorm <21600193@handong.edu>
2018-05-05 01:19:41 +0900

    Update User.java

--- src/cr/usr/User.java
+++ src/cr/usr/User.java
@@ -11,29 +11,47 @@ public class User{
 	private String type;
 	private int age;
 	
+	public User(){}
+	
+	public User(String userId, String userName, String type, int age){
+		this.userId = userId;
+		this.userName = userName;
+		this.type = type;
+		this.age = age;
+	}
+	
 	public String getUserId() {
 		return userId;
 	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+	
 	public String getUserName() {
 		return userName;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	
 	public String getType() {
 		return type;
 	}
-	public void setType(String type) {
-		this.type = type;
-	}
+
 	public int getAge() {
 		return age;
 	}
+	
+	/*
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
+	*/
 
 }
